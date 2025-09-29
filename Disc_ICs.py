d1cc4ceb7b2c9b937402c9b4d04d67a9f5e7cb87
graves-andrew-02 <146735521+graves-andrew-02@users.noreply.github.com>
2025-09-29 14:36:01 +0100

    Update Disc_ICs.py

--- Disc_ICs.py
+++ Disc_ICs.py
@@ -1,38 +1,29 @@
-import numpy as np
-import pandas as pd
-
 # Constants (in appropriate units)
 G = 4 * np.pi**2 #39.47841760435743  # AU^3 / (Msun * yr^2)
-M_star = 1.0           # Msun
+M_star = 5.0           # Msun
 N_particles = 12000
-R_max = 50; R_min = 1   # AU
-mass_particle = 0.1 / N_particles   # placeholder mass per particle
+R_max = 100; R_min = 0   # AU
+mass_particle = 5 / N_particles   # placeholder mass per particle
 
 # Generate radial positions with uniform surface density
 n = 0
 positions = []
 while n < N_particles:
-  test_pos = 2*R_max*(np.random.rand(2)-0.5)
+  test_pos = 2*R_max*(np.random.rand(3)-0.5)
   #rejection test
   if np.sum(test_pos**2)**0.5 <= R_max and np.sum(test_pos**2)**0.5 >= R_min:
     positions.append(test_pos)
     n += 1
 x = np.array([pos[0] for pos in positions])
 y = np.array([pos[1] for pos in positions])
-z = np.zeros(N_particles)#np.array([pos[2] for pos in positions])
+z = np.array([pos[2] for pos in positions])
 
-r = np.sqrt(x**2 + y**2)
-
-theta = np.arctan(y/x)
-
-# Compute Keplerian velocity
-v_circ = np.sqrt(G * M_star / r)
+v_circ = 1
 vx = -v_circ * y/r
 vy = v_circ * x/r
 vz = np.zeros(N_particles)
 
-# Temperature profile: T ∝ r^{-1/2} ⇒ energy per mass ∝ T ∝ r^{-1/2}
-energy = 0.1*(r / R_max) ** -0.5
+energy = 0.25 * np.ones(N_particles)
 mass = np.full(N_particles, mass_particle)
 
 # Create DataFrame and save
@@ -45,6 +36,5 @@ df = pd.DataFrame({
 })
 
 # Save to file
-df.to_csv("disc_12000_2.txt", sep=' ', index=False, float_format='%.15e')
-
-print(np.sqrt(R_max**2/N_particles))
+df.to_csv("Collapse.txt", sep=' ', index=False, float_format='%.15e')
+print(np.sqrt(R_max**3/N_particles))
